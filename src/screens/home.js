790d0355b0b619f32b41bc6e2c19c7ba42fa2af8
Jesus Camacaro <jcamacaro@messangi.com>
2020-01-30 19:10:19 -0300

    stacnavigation

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,6 +1,7 @@
 /* jshint ignore:start */
 import React, { Component } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { StackActions } from 'react-navigation';
 
 class Home extends Component {
     
@@ -9,7 +10,25 @@ class Home extends Component {
         // console.log(this.props.navigation); 
 
         // para navegar entre pantallas
-        this.props.navigation.navigate('Login');
+        // this.props.navigation.navigate('Login');
+
+        // Se puede hacer asi
+        // this.props.navigation.dispatch({
+        //     type: 'NAVIGATE',
+        //     payload: {
+        //         screen: 'Login',
+        //         params: {
+        //             name: 'jesus 23'
+        //         }
+        //     }
+        // });
+
+        // o esto 
+        // para esto se puede usar el pop, y todas las demas
+        this.props.navigation.dispatch( StackActions.push({
+            routeName: 'Login'
+        }) );
+
     }
     
     render() {
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { NavigationActions } from 'react-navigation';
 
 class Login extends Component {
     
     handlePress = () => {
-        this.props.navigation.navigate('About');
+        //this.props.navigation.navigate('About');
         // {this.props.navigation.state.params.nombre}
+
+        // se le puede pasar estos parametros
+        this.props.navigation.dispatch(
+            NavigationActions.navigate({
+                routeName: 'About',
+                // params
+                // key
+                // action
+            })
+        )
     }
 
     render() {
